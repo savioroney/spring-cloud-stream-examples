6f3d3b6b0756ccf5d325a9f3eed3c249e5b9e434
ssoares <savio_soares@gap.com>
2020-04-07 14:34:35 -0300

    Remove unnecessary code

--- multiple-binders/src/test/java/com/example/multiplebinders/MultipleBindersApplicationTests.java
+++ multiple-binders/src/test/java/com/example/multiplebinders/MultipleBindersApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.multiplebinders;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Ignore
 class MultipleBindersApplicationTests {
 
 	@Test
